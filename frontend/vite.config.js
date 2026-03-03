c59a2ae2fafb42c709b2ef55aa55262a9dc6567d
GZ-Alex <AlexMauth2505@web.de>
2026-03-03 18:51:04 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/starbound-alpha/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
